866c88f327af33de2ce183439b81ae005696e53f
Bss <mx.bss@proton.me>
2026-03-25 00:53:20 +0100

    updates

--- tidal/setup/BootTidal.hs
+++ tidal/setup/BootTidal.hs
@@ -1,6 +1,9 @@
 :set -fno-warn-orphans -Wno-type-defaults -XMultiParamTypeClasses -XOverloadedStrings
 :set prompt ""
 :set -package tidal
+:set -package tidal-core
+:set -package containers 
+
 
 -- Import all the boot functions and aliases.
 import Sound.Tidal.Boot
@@ -10,23 +13,29 @@ import qualified Data.Map.Strict    as Map
 
 default (Rational, Integer, Double, Pattern String)
 
-let mspTarget = Target { oName = "msp", oAddress = "127.0.0.1", oHandshake = False, oPort = 7400, oBusPort = Nothing, oLatency = 0.2, oSchedule = Pre BundleStamp, oWindow = Nothing }
-let mspShape = OSC "/tidal/event/" $ Named {requiredArgs = ["s_max"]}
-let s_max = pS "s_max"
-let bus = pI "bus"
+-- let mspTarget = Target { oName = "msp", oAddress = "127.0.0.1", oHandshake = False, oPort = 7400, oBusPort = Nothing, oLatency = 0.2, oSchedule = Pre BundleStamp, oWindow = Nothing }
+-- let mspShape = OSC "/tidal/event/" $ Named {requiredArgs = ["s_max"]}
+-- let s_max = pS "s_max"
+-- let bus = pI "bus"
 
-let editorTarget = Target { oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.02, oSchedule = Live, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
-let editorShape = OSCContext "/editor/highlights"
+let bss_os_Target = Target { oName = "bss-os", oAddress = "127.0.0.1", oHandshake = False, oPort = 7410, oBusPort = Nothing, oLatency = 0.2, oSchedule = Pre BundleStamp, oWindow = Nothing }
+let bss_os_Shape = OSC "/tidal/bss_os/" $ Named {requiredArgs = ["t"]}
+let t = pI "t"
 
--- oscmap = [(mspTarget, [mspShape]), (superdirtTarget { oPort = 57120, oLatency = 0.02 }, [superdirtShape]), (editorTarget, [editorShape])]
+let clockShape = OSC "/ping" $ Named {requiredArgs = ["clock"]}
+let clockTarget = Target {oName = "clock", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.2, oSchedule = Live, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
+
+oscmap = [(bss_os_Target, [bss_os_Shape]), (superdirtTarget { oPort = 57120, oLatency = -0.02 }, [superdirtShape]), (clockTarget, [clockShape])]
 
 -- Create a Tidal Stream with the default settings.
 -- To customize these settings, use 'mkTidalWith' instead
 -- tidalInst <- mkTidal
 -- tidalInst <- mkTidalWith oscmap defaultConfig 
-tidalInst <- mkTidalWith [(superdirtTarget { oPort = 57120, oLatency = 0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20}) 
+-- tidalInst <- mkTidalWith [(superdirtTarget { oPort = 57120, oLatency = 0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20}) 
 
 -- tidalInst <- mkTidalWith [(superdirtTarget { oLatency = 0.01 }, [superdirtShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
+--tidalInst <- mkTidalWith [(superdirtTarget { oLatency = -0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
+tidalInst <- mkTidalWith oscmap (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 0.2})
 
 -- This orphan instance makes the boot aliases work!
 -- It has to go after you define 'tidalInst'.
